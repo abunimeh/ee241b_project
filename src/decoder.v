a066adca9c3eb01cc90f7843a308ec9beebf5748
Angie <shunshou@gmail.com>
2017-02-04 22:20:29 -0800

    change the problem slightly so that the output needs to be registered to pass the TB; encourage short shorter code

--- src/decoder.v
+++ src/decoder.v
@@ -1,21 +1,26 @@
 module decoder(
-  input [2:0] A,
-  output [7:0] Z,
+  input [4:0] A,
+  output [31:0] Z,
   input clk
 );
 
-	
+// NOTE: Testbench expects that the output Z is registered!
+
 // Only one output should ever be high.  For example,
-// Z[2] = !A[2] & A[1] & !A[0], etc
+// Z[2] = !A[4] & !A[3] & !A[2] & A[1] & !A[0], etc
+// When Z[2] is high, Z[0], Z[1], Z[3] to Z[31] should all be low
 
 // Hint: a 2 to 1 mux looks like:
-// assign y = (sel) ? b : a;
+// assign z = (sel) ? b : a;
+// Where z = b when sel is high; otherwise z = a
 // And you can replace "a" with a further condition
-// assign y = (sel1) ? c
+// assign z = (sel1) ? c
 //          : (sel2) ? b
 //          : a
 // and sel1 can be a condition like:
-// A == 3'd0
+// A == 5'd0
+// OR: Since a 5 to 32 decoder using this approach requires a lot of copy + pasting,
+// I challenge you to come up with a shorter way to write the same thing ;)
 
 assign Z  = ;
 
