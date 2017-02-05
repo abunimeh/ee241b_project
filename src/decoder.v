cf2cfc3df065442eb50961ea95f494754c248eeb
Angie <shunshou@gmail.com>
2017-02-04 18:08:11 -0800

    commiting files from 2016 decoder_38

--- src/decoder.v
+++ src/decoder.v
@@ -1,12 +1,12 @@
 module decoder(
-  input [3:0] A,
-  output [15:0] Z,
+  input [2:0] A,
+  output [7:0] Z,
   input clk
 );
 
 	
 // Only one output should ever be high.  For example,
-// Z[2] = !A[3] & !A[2] & A[1] & !A[0], etc
+// Z[2] = !A[2] & A[1] & !A[0], etc
 
 // Hint: a 2 to 1 mux looks like:
 // assign y = (sel) ? b : a;
@@ -15,7 +15,7 @@ module decoder(
 //          : (sel2) ? b
 //          : a
 // and sel1 can be a condition like:
-// A == 4'd0
+// A == 3'd0
 
 assign Z  = ;
 
