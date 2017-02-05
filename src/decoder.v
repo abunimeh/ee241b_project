ee873814f0676415b7dca4d12a8a6921220dbd4f
Angie <shunshou@gmail.com>
2017-02-05 00:28:38 -0800

    get rid of assign Z, b/c you need to register

--- src/decoder.v
+++ src/decoder.v
@@ -7,22 +7,20 @@ module decoder(
 // NOTE: Testbench expects that the output Z is registered!
 
 // Only one output should ever be high.  For example,
-// Z[2] = !A[4] & !A[3] & !A[2] & A[1] & !A[0], etc
+// Z[2] = !A[4] & !A[3] & !A[2] & A[1] & !A[0] of the previous clk cycle, etc
 // When Z[2] is high, Z[0], Z[1], Z[3] to Z[31] should all be low
 
 // Hint: a 2 to 1 mux looks like:
-// assign z = (sel) ? b : a;
-// Where z = b when sel is high; otherwise z = a
+// assign y = (sel) ? b : a;
+// Where y = b when sel is high; otherwise y = a
 // And you can replace "a" with a further condition
-// assign z = (sel1) ? c
+// assign y = (sel1) ? c
 //          : (sel2) ? b
-//          : a
+//          : a ;
 // and sel1 can be a condition like:
 // A == 5'd0
 // OR: Since a 5 to 32 decoder using this approach requires a lot of copy + pasting,
 // I challenge you to come up with a shorter way to write the same thing ;)
-
-assign Z  = ;
 
 endmodule
 
