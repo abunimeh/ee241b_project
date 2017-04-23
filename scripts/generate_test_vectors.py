2d6e429bf89aa44d6e7022828d35a62c56840bb4
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2017-04-23 15:58:52 -0700

    more scripts

--- scripts/generate_test_vectors.py
+++ scripts/generate_test_vectors.py
@@ -13,51 +13,19 @@ Arguments:
         if the directory already exists all files inside it will be removed!
 """
 import sys
-import re
 import shutil
 import os
 import numpy as np
 import bitarray
+from verilog_parser import VerilogParser 
 
 verilog_file_path = sys.argv[1]
 num_sequences = int(sys.argv[2])
 num_vectors_per_sequence = int(sys.argv[3])
 output_directory_path = sys.argv[4]
 
-# Get a listing of the inputs for the circuit as described in the Verilog file
-# Note that this assumes the Verilog-2001 way of declaring inputs
-"""
-module asdf (N1, N2, N3, ...);
-
-input N1, N2,
-    N3;
-"""
-def findWholeWord(w):
-    return re.compile(r'\b({0})\b'.format(w)).search
-
-input_string = ""
-input_section = False
-print("Parsing Verilog file at path: %s" % (verilog_file_path))
-with open(verilog_file_path, 'r') as f:
-    for line in f:
-        if input_section:
-            input_string = input_string + line
-        if findWholeWord('input')(line):
-            input_section = True
-            input_string = input_string + line
-        if ';' in line and input_section == True:
-            break
-
-input_string = input_string.strip()
-input_string = input_string.replace('\r', '')
-input_string = input_string.replace('\n', '')
-input_string = input_string.replace(' ', '')
-input_string = input_string.replace('input', '')
-input_string = input_string.replace(';', '')
-inputs = input_string.split(',')
-
-print("Found these inputs to the Verilog module: %s" % (inputs))
-print("Num inputs: %d" % (len(inputs)))
+parser = VerilogParser(verilog_file_path)
+inputs = parser.inputs
 
 print("Creating output directory (or cleaning it if already exists) at path: %s" % (output_directory_path))
 
---
+++ scripts/verilog_parser.py
@@ -0,0 +1,63 @@
+"""
+This file contains helper functions for parsing a Verilog RTL file.
+
+Note that this assumes the Verilog-2001 way of declaring inputs
+module asdf (N1, N2, N3, ...);
+
+    input N1, N2, N3;
+    output N4, N5, N6;
+
+endmodule
+"""
+import re
+def findWholeWord(w):
+    return re.compile(r'\b({0})\b'.format(w)).search
+
+class VerilogParser(object):
+    def parseForString(self, string):
+        return_string = ""
+        parsing_section = False
+        with open(self.verilog_file_path, 'r') as f:
+            for line in f:
+                if parsing_section:
+                    return_string = return_string + line
+                if findWholeWord(string)(line):
+                    parsing_section = True
+                    return_string = return_string + line
+                if ';' in line and parsing_section == True:
+                    break
+
+        return_string = return_string.replace('\r', '')
+        return_string = return_string.replace('\n', '')
+        return_string = return_string.replace(' ', '')
+        return_string = return_string.replace(string, '')
+        return_string = return_string.replace(';', '')
+        return_value = return_string.split(',')
+        return return_value 
+
+    def findModuleName(self):
+        module_string = ""
+        with open(self.verilog_file_path, 'r') as f:
+            for line in f:
+                if findWholeWord('module')(line):
+                    module_string = line
+                    break
+        module_string = module_string.replace('module', '')
+        module_string = module_string.split('(')
+        return module_string[0].strip()
+    
+    def __init__(self, verilog_file_path):
+        self.verilog_file_path = verilog_file_path
+        print("Created VerilogParser to parse Verilog file at path: %s" % (self.verilog_file_path))
+        self.inputs = self.parseForString('input')
+        self.outputs = self.parseForString('output')
+        self.moduleName = self.findModuleName()
+
+        print("Parsed Verilog module name: %s" % (self.moduleName))
+
+        print("Found these inputs to the Verilog module: %s" % (self.inputs))
+        print("Num inputs: %d" % (len(self.inputs)))
+
+        print("Found these outputs from the Verilog module: %s" % (self.outputs))
+        print("Num outputs: %d" % (len(self.outputs)))
+
