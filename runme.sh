eebd60c8635241b1fd5172cbd0f3b95e00dcd68d
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2017-04-23 20:40:33 -0700

    added runme arguments for num sequences and num vectors per sequence

--- runme.sh
+++ runme.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo usage: runme.sh verilog_module_name
+if [ $# -ne 3 ]; then
+    echo usage: runme.sh verilog_module_name test_sequences vectors_per_sequence
     exit 1
 fi
 
-cp iscas85_verilog/$1.v src/$1.v
-python scripts/generate_test_vectors.py iscas85_verilog/$1.v 10 10 input_test_vectors
-python scripts/testbench_generator.py iscas85_verilog/$1.v input_test_vectors/input_test_vectors 10 src/$1_tb.v
-python scripts/makefile_generator.py $1 build-rvt/MakePowerEstimation
+module_name=$1
+num_seq=$2
+num_vecs=$3
+
+cp iscas85_verilog/$module_name.v src/$module_name.v
+python scripts/generate_test_vectors.py iscas85_verilog/$module_name.v $num_seq $num_vecs input_test_vectors
+python scripts/testbench_generator.py iscas85_verilog/$module_name.v input_test_vectors/input_test_vectors $num_vecs src/${module_name}_tb.v
+python scripts/makefile_generator.py $module_name build-rvt/MakePowerEstimation
 cd build-rvt/vcs-sim-rtl/
 make clean
 make
