6e411b56f5fb66f752b8dfc3c6b5b1e12499274b
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2017-04-29 21:36:06 -0700

    added script to construct several types of power models, quite fast

--- scripts/model_and_predict.sh
+++ scripts/model_and_predict.sh
@@ -1,22 +1,5 @@
-#python gather_power_data.py c17 300 30 ../data/c17_training
-#python gather_power_data.py c17 300 30 ../data/c17_testing
-#python gather_power_data.py c432 300 30 ../data/c432_training
-#python gather_power_data.py c432 300 30 ../data/c432_testing
-#python gather_power_data.py c499 300 30 ../data/c499_training
-#python gather_power_data.py c499 300 30 ../data/c499_testing
-#python gather_power_data.py c880 300 30 ../data/c880_training
-#python gather_power_data.py c880 300 30 ../data/c880_testing
-#python gather_power_data.py c1908 300 30 ../data/c1908_training
-#python gather_power_data.py c1908 300 30 ../data/c1908_testing
-#python gather_power_data.py c2670 300 30 ../data/c2670_training
-#python gather_power_data.py c2670 300 30 ../data/c2670_testing
-#python gather_power_data.py c6288 300 30 ../data/c6288_training
-#python gather_power_data.py c6288 300 30 ../data/c6288_testing
-#python gather_power_data.py c7552 300 30 ../data/c7552_training
-#python gather_power_data.py c7552 300 30 ../data/c7552_testing
-
-python construct_power_model.py ../data/c17_training/sequences ../data/c17_power_model
-python power_model_prediction.py ../data/c17_testing/sequences ../data/c17_power_model/power_model ../data/c17_power_model/
-
-python construct_power_model.py ../data/c432_training/sequences ../data/c432_power_model
-python power_model_prediction.py ../data/c432_testing/sequences ../data/c432_power_model/power_model ../data/c432_power_model/
+for module in c17; do
+    for model in 4d_table linear quadratic cubic; do
+        python construct_power_model.py ../data/${module}_training/sequences ${model} ../models/${module}_power_model ${model}_power_model
+    done
+done
